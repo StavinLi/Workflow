6806b89083899a8047447c95bfba2f54015cd101
lichen <495727881@qq.com>
2020-06-10 09:50:34 +0800

    dist

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-    mode: 'history',
     routes: [{
         path: '/',
         redirect: '/approval/setting'
--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ function resolve(dir) {
 module.exports = {
     lintOnSave: false,
     productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === "production" ? '/Workflow/' : '/',
     devServer: {
         disableHostCheck: true,
         https: false, // https:{type:Boolean}
