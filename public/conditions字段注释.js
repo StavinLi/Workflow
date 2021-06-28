302611a01cd49ca12d45a286a5e337034e5e1ec1
lichen <495727881@qq.com>
2021-06-28 11:29:05 +0800

    add:conditions字段注释

--- public/conditions字段注释.js
+++ public/conditions字段注释.js
@@ -1,12 +1,15 @@
-{
+export default {
   "code": "200",
   "msg": "操作成功",
   "data": [
     {
+      //条件id  columnId == 0 为发起人
       "columnId": "1090",
-      "showType": "1",
+      //columnType == "String" && showType == "3"为多选  columnType == "Double"为区间
+      "showType": "1", 
       "showName": "预计采购总费用",
       "columnType": "Double",
+      //fixedDownBoxValue 匹配 columnType == "String" && showType == "3"时子选项内容
       "fixedDownBoxValue": ""
     },
     {
@@ -14,7 +17,7 @@
       "showType": "3",
       "showName": "采购类型",
       "columnType": "String",
-      "fixedDownBoxValue": "{\"1\":{\"key\":\"1\",\"value\":\"新开园区集中采购\",\"column\":\"\",\"type\":\"1\"}}"
+      "fixedDownBoxValue": "{\"1\":{\"key\":\"1\",\"value\":\"新开园区集中采购\",\"column\":\"\",\"type\":\"1\"}}" 
     },
     {
       "columnId": "1093",
