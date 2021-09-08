dfc5a4d0a77bc541f47741b01565c937357c639f
lichen <495727881@qq.com>
2021-09-08 21:37:45 +0800

    update:Gitee Pages

--- dist/conditions字段注释.js
+++ dist/conditions字段注释.js
@@ -8,6 +8,8 @@ export default {
       //columnType == "String" && showType == "3"为多选  columnType == "Double"为区间
       "showType": "1", 
       "showName": "预计采购总费用",
+      //columnName 条件自定义字段
+      "columnName": "planProcurementTotalMoney",
       "columnType": "Double",
       //fixedDownBoxValue 匹配 columnType == "String" && showType == "3"时子选项内容
       "fixedDownBoxValue": ""
@@ -16,6 +18,7 @@ export default {
       "columnId": "1092",
       "showType": "3",
       "showName": "采购类型",
+      "columnName": "procurementType",
       "columnType": "String",
       "fixedDownBoxValue": "{\"1\":{\"key\":\"1\",\"value\":\"新开园区集中采购\",\"column\":\"\",\"type\":\"1\"}}" 
     },
@@ -23,6 +26,7 @@ export default {
       "columnId": "1093",
       "showType": "1",
       "showName": "园区面积",
+      "columnName": "parkArea",
       "columnType": "Double",
       "fixedDownBoxValue": ""
     }
